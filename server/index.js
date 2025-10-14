a4b726a5269b3ecab1b6a3be0a7e95185985f498
Eevolxy <uammeloot@gmail.com>
2025-10-14 17:43:26 +0200

    Accès aux fichiers static pour express et configuration du moteur de vue Handlebars

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import recipesRoutes from './routes/recipes.js'
+import path from "path";
+import {engine} from "express-handlebars";
 
 const app = express();
 const PORT = 3000
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.set('views', path.join(__dirname, '..', 'client', 'views'))
+app.use('/static', express.static(path.join(__dirname, '..', 'client', 'static')))
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
 app.use("/api/recipes", recipesRoutes)
 
