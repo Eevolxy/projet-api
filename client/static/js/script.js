e7a5d98fd328790fc3a8424fa9486184663146cb
Fox-Programs <alexis.facon@ynov.com>
2025-10-23 17:49:30 +0200

    Add login and registration pages with corresponding styles and functionality

--- client/static/js/script.js
+++ client/static/js/script.js
@@ -96,5 +96,6 @@ returnArrow.addEventListener('click', () => {
     titlePage.classList.remove('hidden')
     divItems.classList.remove('hidden')
     divInput.value = ''
+    divItems.textContent = ''
     displayCards(listName, listImg);
 });
